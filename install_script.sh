fb50629242507ef761c66f3af857ba815837cbb8
Avonae <127596994+Avonae@users.noreply.github.com>
2024-10-09 20:28:14 +0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -24,6 +24,4 @@ chown -R 503:503 /data/teamspeak
 
 # Запускаем контейнер с тимсписком
 docker run -d --restart=always --name teamspeak -e PUID=503 -e PGID=503 -e TS3SERVER_GDPR_SAVE=false -e TS3SERVER_LICENSE=accept -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 -v /data/teamspeak:/data mbentley/teamspeak
-  
-# Ждем запуска контейнера и получаем токен администратора
-echo "Ожидание запуска TeamSpeak сервера..."
+
