85971a150587e91c8827ae76315b2f8d94a39bdf
Avonae <127596994+Avonae@users.noreply.github.com>
2024-10-10 13:15:57 +0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -5,6 +5,7 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 # Добавляем текущего юзера в переменную
 CURRENT_USER=$USER
+
 # Добавляем ключи докера
 sudo apt-get install ca-certificates 
 sudo install -m 0755 -d /etc/apt/keyrings
