50ee767e1350a112aef561fe93b24957c0c9580c
Avonae <127596994+Avonae@users.noreply.github.com>
2024-10-23 13:03:16 +0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Получаем IP-адрес сервера
+# Getting server IP address
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# Добавляем ключи докера
+# Adding docker keys
 sudo apt-get install ca-certificates 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Добавляем пакеты докера:
+# Adding docker packages
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Обновляем систему
+# System updating
 sudo apt update && apt upgrade -y
-# ставим докер
+# Docker installation
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Создаем папку для пользовательских данных
+# Creating a folder for user data
 mkdir -p /data/teamspeak
-# Назначаем владельцем папки бесправного пользователя
+# Make the 503 user an owner of the folder
 chown -R 503:503 /data/teamspeak
 
-# Запускаем контейнер с тимсписком
+# Starting teamspeak container
 docker run -d --restart=always --name teamspeak -e PUID=503 -e PGID=503 -e TS3SERVER_GDPR_SAVE=false -e TS3SERVER_LICENSE=accept -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 -v /data/teamspeak:/data mbentley/teamspeak
 
-# Ждем 15 секунд
+# Wait 15 sec
 sleep 15
 
-# Ищем строку с токеном в логах
+# Searching admin token in logs
 ADMIN_TOKEN=$(grep -r "token=" /data/teamspeak/logs | awk -F'token=' '{print $2}')
 
-# Выводим токен администратора
+# Show admin token
 echo "------------------------------------------------------------------"
-echo "Сервер TeamSpeak установлен и запущен."
-echo "Ваш токен администратора: $ADMIN_TOKEN"
-echo "IP-адрес сервера для подключения: $IP_ADDRESS"
+echo "TeamSpeak server has been installed and started."
+echo "Your admin token is: $ADMIN_TOKEN"
+echo "IP-address for connect to server: $IP_ADDRESS"
